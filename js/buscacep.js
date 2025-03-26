f8f72df694ef4f010996a3874fb4eaf8700b7aa1
JOSE LUCAS BENTO FLEURY <jose.lbfleury@senacsp.edu.br>
2025-03-25 22:25:09 -0300

    Chega por hj!!

---
+++ js/buscacep.js
@@ -0,0 +1,15 @@
+function LimparCep(){
+    document.getElementsByClassName("inputCEP")[0].value = "";
+    document.getElementsByClassName("inputLogradouro")[0].value = "";
+    document.getElementsByClassName("inputComplemento")[0].value = "";
+    document.getElementsByClassName("inputNumero")[0].value = "";
+    document.getElementsByClassName("inputBairro")[0].value = "";
+    document.getElementsByClassName("inputLocalidade")[0].value = "";
+    document.getElementsByClassName("inputUF")[0].value = "";
+
+    document.getElementsByClassName("inputCEP")[0].focus();
+}
+
+function BuscarCep(){
+
+}
--- js/calculadora.js
+++ js/calculadora.js
@@ -1,33 +1,44 @@
 var valor1 = 0;
 var valor2 = 0;
+var operacao = "";
 
 
 function soma(){
     valor1 = parseFloat(document.getElementsByClassName("inputVisorCalculadora")[0].value);
     Limpar();
+    operacao = "soma";
 }
 
 function subtracao(){
     valor1 = parseFloat(document.getElementsByClassName("inputVisorCalculadora")[0].value);
     Limpar();
+    operacao = "subtracao";
 
 }
 
 function multiplicacao(){
     valor1 = parseFloat(document.getElementsByClassName("inputVisorCalculadora")[0].value);
     Limpar();
+    operacao = "multiplicacao";
 
 }
 
 function divisao(){
     valor1 = parseFloat(document.getElementsByClassName("inputVisorCalculadora")[0].value);
     Limpar();
-
+    operacao = "divisao";
 }
 
 
-function Limpar(){
+function LimparTudo(){
  document.getElementsByName("visorCalculadora")[0].value = 0;
+ valor1 = 0;
+ valor2 = 0;
+ operacao = "";
+}
+
+function Limpar(){
+    document.getElementsByName("visorCalculadora")[0].value = 0;
 }
 
 function numero0(){
@@ -38,53 +49,68 @@ function numero0(){
 function numero1(){
     let visor = document.getElementsByName("visorCalculadora")[0];
    visor.value += 1
-
 }
 
 function numero2(){
     let visor = document.getElementsByName("visorCalculadora")[0];
     visor.value += 2
-
 }
 
 function numero3(){
     let visor = document.getElementsByName("visorCalculadora")[0];
     visor.value += 3
-
 }
 
 function numero4(){
     let visor = document.getElementsByName("visorCalculadora")[0];
     visor.value += 4
-
 }
 
 function numero5(){
     let visor = document.getElementsByName("visorCalculadora")[0];
     visor.value += 5
-
 }
 
 function numero6(){
     let visor = document.getElementsByName("visorCalculadora")[0];
     visor.value += 6
-
 }
 
 function numero7(){
     let visor = document.getElementsByName("visorCalculadora")[0];
     visor.value += 7
-
 }
 
 function numero8(){
     let visor = document.getElementsByName("visorCalculadora")[0];
     visor.value += 8
-
 }
 
 function numero9(){
     let visor = document.getElementsByName("visorCalculadora")[0];
     visor.value += 9
+}
 
+function calcular(){
+
+    let visor = document.getElementsByName("visorCalculadora")[0];
+    valor2 = parseFloat(visor.value);
+    resultado = 0;
+
+    switch(operacao){
+        case "soma":
+            resultado = valor1 + valor2;
+            break;
+        case "subtracao":
+            resultado = valor1 - valor2;
+            break;
+        case "multiplicacao":
+            resultado = valor1 * valor2;
+            break;
+        case "divisao":
+            resultado = valor1 / valor2;
+            break;
+    }
+
+    visor.value= resultado;
 }
